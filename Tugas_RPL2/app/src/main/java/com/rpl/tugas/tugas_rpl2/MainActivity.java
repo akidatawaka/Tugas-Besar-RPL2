39851094ae4a9980a117c18e1c1243c37ed32bcd
Deden Masmunadar <dmasmunandar@gmail.com>
2016-05-11 19:00:31 +0700

    Pertama
    Pertama

---
+++ Tugas_RPL2/app/src/main/java/com/rpl/tugas/tugas_rpl2/MainActivity.java
@@ -0,0 +1,40 @@
+package com.rpl.tugas.tugas_rpl2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final Animation animScale = AnimationUtils.loadAnimation(this, R.anim.anim_scale);
+
+
+        ImageButton btnZoom = (ImageButton) findViewById(R.id.button_main);
+        ImageButton btnZoom2 = (ImageButton) findViewById(R.id.button_pengaturan);
+
+
+        btnZoom.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animScale);
+            }});
+
+        btnZoom2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                v.startAnimation(animScale);
+            }});
+
+
+
+    }
+}
